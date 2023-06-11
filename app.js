d9edd22fede6e67186c4287ba297ca22853721e7
ramiro <navarretee1133@gmail.com>
2023-06-11 19:41:34 -0300

    nuevo

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express'
 import path from 'path'
+<<<<<<< HEAD
+=======
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+>>>>>>> master
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -18,6 +23,7 @@ app.use(morgan('dev'))
 //sirve para los formularios envio de informacion al node js
 app.use(bodyParser.urlencoded({ extended: true }))
 
+<<<<<<< HEAD
 //motor de plantilla
 app.set('view engine', 'ejs')
 
@@ -27,6 +33,22 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render('pages/index.ejs')
 })
+=======
+// Obtener la ruta del directorio actual
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+//motor de plantilla
+app.set('views', path.join(currentDir, 'views'));
+app.set('view engine', 'ejs');
+
+// folder public - en teoria es un middleware
+app.use(express.static('public'))
+
+// routes
+app.get('/articulos', (req, res) => {
+    res.render('pages/articulos/index');
+});
+>>>>>>> master
 
 
 
