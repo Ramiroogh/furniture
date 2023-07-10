017b910848ce00e190336bd5d693b5b582a72c24
ramiro <navarretee1133@gmail.com>
2023-07-10 17:15:08 -0300

    BUSCADOR IMPLEMENTADO

--- src/routes/articulosRoutes.js
+++ src/routes/articulosRoutes.js
@@ -6,8 +6,26 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const productos = await Producto.find({}).exec();
-    res.render('pages/articulos/index.ejs', { productos: productos });
+    const { categoria, busqueda } = req.query;
+    let query = {};
+
+    if (categoria) {
+      const regex = new RegExp(categoria, 'i');
+      query.categoria = regex;
+    }
+
+    if (busqueda) {
+      const regex = new RegExp(busqueda, 'i');
+      query.$or = [
+        { nombre: regex },
+        { categoria: regex }
+      ];
+    }
+
+    const productos = await Producto.find(query).exec();
+    const valorBusqueda = busqueda || '';
+
+    res.render('pages/articulos/index.ejs', { productos, valorBusqueda });
   } catch (error) {
     console.error('Error al obtener los productos:', error);
     res.status(500).send('Error al obtener los productos');
